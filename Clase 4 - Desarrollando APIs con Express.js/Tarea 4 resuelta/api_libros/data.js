db101f8f005d68744e2d652a640f97c404b9d33b
trechiStrix <matydavila_77@hotmail.com>
2023-06-22 19:41:25 -0300

    class 4 completed

---
+++ Clase 4 - Desarrollando APIs con Express.js/Tarea 4 resuelta/api_libros/data.js
@@ -0,0 +1,15 @@
+const libros = [
+    { id: '1', titulo: '1984', autor: 'George Orwell' },
+    { id: '2', titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez' },
+    { id: '3', titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes Saavedra' },
+    { id: '4', titulo: 'Orgullo y prejuicio', autor: 'Jane Austen' },
+    { id: '5', titulo: 'Matar a un ruiseñor', autor: 'Harper Lee' },
+    { id: '6', titulo: 'En busca del tiempo perdido', autor: 'Marcel Proust' },
+    { id: '7', titulo: 'Ulises', autor: 'James Joyce' },
+    { id: '8', titulo: 'Crimen y castigo', autor: 'Fyodor Dostoevsky' },
+    { id: '9', titulo: 'El Gran Gatsby', autor: 'F. Scott Fitzgerald' },
+    { id: '10', titulo: 'Donde los árboles cantan', autor: 'Laura Gallego' },
+    { id: '11', titulo: 'La sombra del viento', autor: 'Carlos Ruiz Zafón' },
+    { id: '12', titulo: 'Harry Potter y la piedra filosofal', autor: 'J.K. Rowling' }];
+
+    module.exports = libros;

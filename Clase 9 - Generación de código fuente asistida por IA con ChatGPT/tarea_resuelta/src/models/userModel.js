22e16cdcbe950b97814789d74cc0c0a7c6c6b348
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-13 16:37:09 -0300

    user schema created

---
+++ Clase 9 - Generación de código fuente asistida por IA con ChatGPT/tarea_resuelta/src/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const mongoURI = process.env.MONGO_DB;
+
+mongoose.connect(process.env.MONGO_DB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    id: String,
+    email: String,
+  },
+  { collection: "users" }
+);
+
+const User = mongoose.model("User", LibroSchema);
+
+module.exports = User;
